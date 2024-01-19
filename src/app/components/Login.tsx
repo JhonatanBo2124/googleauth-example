2a280b51a8e1a38c14af007732a6281242f3ce02
Jhonatan Bolivar <jhonatanbolivar14@gmail.com>
2024-01-19 09:40:37 -0500

    decode credentials for save information

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -1,12 +1,26 @@
 import { GoogleLogin, GoogleCredentialResponse } from '@react-oauth/google'
 import decodeJWT from '../utils/decodeJWT';
+import { useState } from 'react';
+
+interface info {
+    name: string,
+    email: string,
+    picture: string
+}
+
 export default function Login() {
+
+    let [ info, setInfo ] = useState<info | null>()
 
     const handleSuccess = (GoogleCredentialResponse: GoogleCredentialResponse) => {
         console.log(GoogleCredentialResponse);
         if(GoogleCredentialResponse.credential){
             const { payload } = decodeJWT(GoogleCredentialResponse.credential);
+            const { name, email, picture } = payload
             console.log(payload);
+            setInfo({ name, email, picture })
+            console.log(info);
+            
         }
         
     }
@@ -15,6 +29,13 @@ export default function Login() {
     }
 
     return(
-        <GoogleLogin onError={handleError} onSuccess={handleSuccess} />
+        <>
+        { !info ? <GoogleLogin useOneTap onError={handleError} onSuccess={handleSuccess} />
+        : <div className='flex flex-col gap-5 items-center'>
+            <img src={info.picture} alt="perfil" className='rounded-full' />
+            <h1 className='text-2xl font-bold'>{info.name}</h1>
+            <p>{info.email}</p>
+            </div>}
+        </>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <GoogleOAuthProvider clientId='830167185295-o5s3b8hnjae0gk3f69ga3kbbipav0dfg.apps.googleusercontent.com'>
       <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className="flex flex-col gap-10 items-center">
-          <h1 className='font-bold text-3xl'>Google auth</h1>
+        <div className="flex flex-col gap-20 items-center">
+          <h1 className='font-bold text-6xl'>Google Oauth</h1>
           <Login/>
         </div>
       </main>
